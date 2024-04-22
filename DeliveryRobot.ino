0bef6dcd6c9573e0effb9db06df9566ff4e46d2a
Max Knyazev <58640177+MaxiEnergy@users.noreply.github.com>
2024-04-22 16:59:31 +0300

    Full code for ESP32-C3

---
+++ DeliveryRobot.ino
@@ -0,0 +1,130 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// задаем имена для портов
+#define IN1 7
+#define IN2 5
+#define IN3 1
+#define IN4 0
+
+// ШИМ каналы
+#define CHANNEL_IN1 0
+#define CHANNEL_IN2 1
+#define CHANNEL_IN3 2
+#define CHANNEL_IN4 3
+
+// Частота ШИМ и разрядность
+const int freq = 5000;
+const int resolution = 8;
+int ledPin = 10;
+
+bool deviceConnected;
+
+BLECharacteristic* pMoveCharacteristic;
+
+BLEServer* pServer;
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        deviceConnected = true;
+    };
+    void onDisconnect(BLEServer* pServer) {
+        deviceConnected = false;
+    }
+};
+
+class MoveCharacteristicCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* pCharacteristic) {
+        std::string value = pCharacteristic->getValue();
+        if (value.length() > 0) {
+            switch (value[0]) {
+                case 0x01: // Влево
+                    ledcWrite(CHANNEL_IN1, 255); // 100% скорость
+                    ledcWrite(CHANNEL_IN2, 0);
+                    ledcWrite(CHANNEL_IN3, 255);
+                    ledcWrite(CHANNEL_IN4, 0);
+                    break;
+                case 0x02: // Вправо
+                    ledcWrite(CHANNEL_IN1, 0);
+                    ledcWrite(CHANNEL_IN2, 255); 
+                    ledcWrite(CHANNEL_IN3, 0);
+                    ledcWrite(CHANNEL_IN4, 255);
+                    break;
+                case 0x05: // Назад
+                    ledcWrite(CHANNEL_IN1, 255); 
+                    ledcWrite(CHANNEL_IN2, 0);
+                    ledcWrite(CHANNEL_IN3, 0);
+                    ledcWrite(CHANNEL_IN4, 255);
+                    break;
+                case 0x04: // Вперед
+                    ledcWrite(CHANNEL_IN1, 0);
+                    ledcWrite(CHANNEL_IN2, 255); 
+                    ledcWrite(CHANNEL_IN3, 255);
+                    ledcWrite(CHANNEL_IN4, 0);
+                    break;
+                case 0x00:
+                    ledcWrite(CHANNEL_IN1, 0);
+                    ledcWrite(CHANNEL_IN2, 0);
+                    ledcWrite(CHANNEL_IN3, 0);
+                    ledcWrite(CHANNEL_IN4, 0);
+                    break;
+                case 0x03:
+                    digitalWrite(ledPin, HIGH);
+                    break;
+                case 0x06:
+                    digitalWrite(ledPin, LOW);
+                    break;
+                default:
+                    ledcWrite(CHANNEL_IN1, 0);
+                    ledcWrite(CHANNEL_IN2, 0);
+                    ledcWrite(CHANNEL_IN3, 0);
+                    ledcWrite(CHANNEL_IN4, 0);
+                    digitalWrite(ledPin, LOW);
+                    break;
+            }
+        }
+    }
+};
+
+void setup() {
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+
+    // Настройка ШИМ
+    ledcSetup(CHANNEL_IN1, freq, resolution);
+    ledcSetup(CHANNEL_IN2, freq, resolution);
+    ledcSetup(CHANNEL_IN3, freq, resolution);
+    ledcSetup(CHANNEL_IN4, freq, resolution);
+    
+    // Привязка пинов к ШИМ каналам
+    ledcAttachPin(IN1, CHANNEL_IN1);
+    ledcAttachPin(IN2, CHANNEL_IN2);
+    ledcAttachPin(IN3, CHANNEL_IN3);
+    ledcAttachPin(IN4, CHANNEL_IN4);
+
+    // Инициализация BLE
+    BLEDevice::init("Yandex Delivery Robot");
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+    BLEService* pMoveService = pServer->createService(BLEUUID((uint16_t)0x170D));
+    pMoveCharacteristic = pMoveService->createCharacteristic(BLEUUID((uint16_t)0x2A60), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+    pMoveCharacteristic->setCallbacks(new MoveCharacteristicCallbacks());
+    pMoveService->start();
+    BLEAdvertising* pAdvertising = pServer->getAdvertising();
+    pAdvertising->addServiceUUID(pMoveService->getUUID());
+    pAdvertising->setScanResponse(false);
+    pAdvertising->setMinPreferred(0x06);
+    pAdvertising->setMinPreferred(0x12);
+    BLEDevice::startAdvertising();
+}
+
+void loop() {
+    if (!deviceConnected) {
+        BLEDevice::startAdvertising();
+    } 
+    delay(1000);
+}
